50a8d4d24d94b144497e4b303f546661b8dbb752
lily <1595135476@qq.com>
2018-09-10 13:35:06 +0800

    all

---
+++ drag-project/js/new-data.js
@@ -0,0 +1,34 @@
+[
+	[{
+		"key": "serial-num"
+	}, {
+		"key": "barcode"
+	}],
+	[{
+		"key": "table",
+		"table": [{
+			"key": "table-number"
+		}, {
+			"key": "table-count"
+		}, {
+			"key": "table-company"
+		}, {
+			"key": "table-price"
+		}, {
+			"key": "table-code"
+		}, {
+			"key": "table-model"
+		}]
+	}],
+	[{
+		"key": "input_title",
+		"value": "每次考试"
+	}],
+	[{
+		"key": "inventory-addr"
+	}, {
+		"key": "color"
+	}, {
+		"key": "quantity"
+	}]
+]
